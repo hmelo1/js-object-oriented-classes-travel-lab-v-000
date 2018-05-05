635e293335e56589c10c6a597b1205287591d078
Eddie Melo <heriberto@melo.nyc>
2018-05-05 00:58:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ class Route{
 
   blocksTravelled(){
     let blocks = 0;
-    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-  }
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+  };
 }
